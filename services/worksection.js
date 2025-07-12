48f65380bdbfba9fd1c473da558d9a08cae5a8f2
eneca-dev <development.department.eneca@gmail.com>
2025-07-12 16:39:35 +0400

    Fix Worksection API v2 integration - use GET requests, correct URL /api/admin/v2/, proper MD5 hash generation

--- services/worksection.js
+++ services/worksection.js
@@ -1,21 +1,40 @@
 const axios = require('axios');
+const crypto = require('crypto-js');
 const { config } = require('../config/env');
 const logger = require('../utils/logger');
 
 class WorksectionService {
   constructor() {
-    this.baseUrl = `https://${config.worksection.domain}/api/admin/`;
-    this.hash = config.worksection.hash;
+    this.baseUrl = `https://${config.worksection.domain}/api/admin/v2/`;
+    this.apiKey = config.worksection.hash; // Это теперь API ключ, а не готовый хеш
+  }
+  
+  // Формирование MD5 хеша согласно документации
+  generateHash(queryParams) {
+    const hashInput = queryParams + this.apiKey;
+    return crypto.MD5(hashInput).toString();
   }
   
   async request(action, params = {}) {
     try {
-      const url = `${this.baseUrl}${action}`;
-      const data = { ...params, hash: this.hash };
+      // Формируем query параметры
+      const queryParams = new URLSearchParams({ action, ...params });
+      const queryString = queryParams.toString();
+      
+      // Генерируем хеш из query параметров + API ключ
+      const hash = this.generateHash(queryString);
+      
+      // Добавляем хеш к query параметрам
+      queryParams.append('hash', hash);
+      
+      // Формируем полный URL
+      const url = `${this.baseUrl}?${queryParams.toString()}`;
       
       logger.info(`Worksection API: ${action}`);
+      logger.info(`Request URL: ${url.replace(/hash=[^&]+/, 'hash=***')}`); // Скрываем хеш в логах
       
-      const response = await axios.post(url, data);
+      // Делаем GET запрос
+      const response = await axios.get(url);
       
       if (response.data.status !== 'ok') {
         throw new Error(response.data.message || 'Unknown API error');
